be04460dc60c1aff07efa8b2fcd59dc7d43d4c89
David <david.jaenson@gmail.com>
2016-10-28 00:21:03 +0200

    Added code formatting

--- cpu/time-equihash.c
+++ cpu/time-equihash.c
@@ -14,12 +14,14 @@
 #include "blake/blake2.h"
 #include "thpool/thpool.h"
 
-double get_time() {
-    //struct timespec ts;
-    //clock_gettime(CLOCK_MONOTONIC, &ts);
-    struct timeval ts;
-    gettimeofday(&ts, NULL);
-    return ts.tv_sec + ts.tv_usec / 1000000.0;
+double
+get_time ()
+{
+  //struct timespec ts;
+  //clock_gettime(CLOCK_MONOTONIC, &ts);
+  struct timeval ts;
+  gettimeofday (&ts, NULL);
+  return ts.tv_sec + ts.tv_usec / 1000000.0;
 }
 
 
@@ -27,47 +29,53 @@ typedef struct element_indice element_indice_t;
 typedef struct element element_t;
 typedef struct bucket bucket_t;
 
-void equihash_init_buckets(bucket_t**, bucket_t**, element_indice_t*** indices);
+void equihash_init_buckets (bucket_t **, bucket_t **,
+			    element_indice_t *** indices);
 
-size_t equihash(uint32_t dst_solutions[20][512], const blake2b_state*, bucket_t*, bucket_t*, element_indice_t** indices);
+size_t equihash (uint32_t dst_solutions[20][512], const blake2b_state *,
+		 bucket_t *, bucket_t *, element_indice_t ** indices);
 
 
 /*
     FROM JOHN TROMP's implementation
 */
-void create_header(blake2b_state *ctx, const char *header, size_t header_size, uint32_t nce) {
-    uint32_t le_N = 200;
-    uint32_t le_K = 9;
-    uint8_t personal[] = "ZcashPoW01230123";
-    memcpy(personal+8,  &le_N, 4);
-    memcpy(personal+12, &le_K, 4);
-    blake2b_param P[1];
-    P->digest_length = (512/200)*200/8;
-    P->key_length    = 0;
-    P->fanout        = 1;
-    P->depth         = 1;
-    P->leaf_length   = 0;
-    P->node_offset   = 0;
-    P->node_depth    = 0;
-    P->inner_length  = 0;
-    memset(P->reserved, 0, sizeof(P->reserved));
-    memset(P->salt,     0, sizeof(P->salt));
-    memcpy(P->personal, (const uint8_t *)personal, 16);
-    blake2b_init_param(ctx, P);
-    blake2b_update(ctx, (const uint8_t *)header, header_size);
-    uint8_t nonce[32];
-    memset(nonce, 0, 32);
-    uint32_t le_nonce = nce;
-    memcpy(nonce,  &le_nonce, 4);
-    blake2b_update(ctx, nonce, 32);
+void
+create_header (blake2b_state * ctx, const char *header, size_t header_size,
+	       uint32_t nce)
+{
+  uint32_t le_N = 200;
+  uint32_t le_K = 9;
+  uint8_t personal[] = "ZcashPoW01230123";
+  memcpy (personal + 8, &le_N, 4);
+  memcpy (personal + 12, &le_K, 4);
+  blake2b_param P[1];
+  P->digest_length = (512 / 200) * 200 / 8;
+  P->key_length = 0;
+  P->fanout = 1;
+  P->depth = 1;
+  P->leaf_length = 0;
+  P->node_offset = 0;
+  P->node_depth = 0;
+  P->inner_length = 0;
+  memset (P->reserved, 0, sizeof (P->reserved));
+  memset (P->salt, 0, sizeof (P->salt));
+  memcpy (P->personal, (const uint8_t *) personal, 16);
+  blake2b_init_param (ctx, P);
+  blake2b_update (ctx, (const uint8_t *) header, header_size);
+  uint8_t nonce[32];
+  memset (nonce, 0, 32);
+  uint32_t le_nonce = nce;
+  memcpy (nonce, &le_nonce, 4);
+  blake2b_update (ctx, nonce, 32);
 }
 
 
-typedef struct equihash_thread_arg {
-    blake2b_state state;
-    bucket_t* src;
-    bucket_t* dst;
-    element_indice_t** indices;
+typedef struct equihash_thread_arg
+{
+  blake2b_state state;
+  bucket_t *src;
+  bucket_t *dst;
+  element_indice_t **indices;
 } equihash_thread_arg_t;
 
 static size_t n_solutions = 0;
@@ -76,73 +84,84 @@ static double start_time = 0;
 static double min_time = 0xfffffff;
 static double max_time = 0;
 
-void threaded_equihash(void* targ) {
-    uint32_t solutions[20][512];
-    equihash_thread_arg_t* arg = targ;
-    double st = get_time();
-    size_t tmp = equihash(solutions, &arg->state, arg->src, arg->dst, arg->indices);
-    double t = get_time() - st;
-    pthread_mutex_lock(&mutex);
-    //printf("!!!num solutions: %u\n", tmp);
-    min_time = t < min_time ? t : min_time;
-    max_time = t > max_time ? t : max_time;
-    n_solutions += tmp;
-    pthread_mutex_unlock(&mutex);
+void
+threaded_equihash (void *targ)
+{
+  uint32_t solutions[20][512];
+  equihash_thread_arg_t *arg = targ;
+  double st = get_time ();
+  size_t tmp =
+    equihash (solutions, &arg->state, arg->src, arg->dst, arg->indices);
+  double t = get_time () - st;
+  pthread_mutex_lock (&mutex);
+  //printf("!!!num solutions: %u\n", tmp);
+  min_time = t < min_time ? t : min_time;
+  max_time = t > max_time ? t : max_time;
+  n_solutions += tmp;
+  pthread_mutex_unlock (&mutex);
 }
 
-int main(int argc, char** argv) {
-    uint32_t t = 1000;
+int
+main (int argc, char **argv)
+{
+  uint32_t t = 1000;
 
-    if(argc < 3) {
-        fprintf(stderr, "%s <iterations> <number of threads>\n", argv[0]);
-        exit(1);
+  if (argc < 3)
+    {
+      fprintf (stderr, "%s <iterations> <number of threads>\n", argv[0]);
+      exit (1);
     }
 
-    size_t iterations = strtoul(argv[1], NULL, 10);
-    size_t num_threads = strtoul(argv[2], NULL, 10);
+  size_t iterations = strtoul (argv[1], NULL, 10);
+  size_t num_threads = strtoul (argv[2], NULL, 10);
 
-    printf("running %zu threads\n", num_threads);
-    threadpool thpool = thpool_init(num_threads);
-    double total_time = 0;
-    n_solutions = 0;
-    equihash_thread_arg_t thread_args[num_threads*iterations];
-    for(size_t i = 0; i < num_threads*2; ++i) {
-        equihash_init_buckets(&thread_args[i].src, &thread_args[i].dst, &thread_args[i].indices);
+  printf ("running %zu threads\n", num_threads);
+  threadpool thpool = thpool_init (num_threads);
+  double total_time = 0;
+  n_solutions = 0;
+  equihash_thread_arg_t thread_args[num_threads * iterations];
+  for (size_t i = 0; i < num_threads * 2; ++i)
+    {
+      equihash_init_buckets (&thread_args[i].src, &thread_args[i].dst,
+			     &thread_args[i].indices);
     }
 
-    start_time = get_time();
-    for(size_t i = 0; i < num_threads*iterations; ++i) {
-        blake2b_state curr_state;
-        create_header(&curr_state, "", 0, t);
-        t++;
-        thread_args[i].state = curr_state;
-        thread_args[i].src = thread_args[i % (2*num_threads)].src;
-        thread_args[i].dst = thread_args[i % (2*num_threads)].dst;
-        thread_args[i].indices = thread_args[i % (2*num_threads)].indices;
+  start_time = get_time ();
+  for (size_t i = 0; i < num_threads * iterations; ++i)
+    {
+      blake2b_state curr_state;
+      create_header (&curr_state, "", 0, t);
+      t++;
+      thread_args[i].state = curr_state;
+      thread_args[i].src = thread_args[i % (2 * num_threads)].src;
+      thread_args[i].dst = thread_args[i % (2 * num_threads)].dst;
+      thread_args[i].indices = thread_args[i % (2 * num_threads)].indices;
 
-        thpool_add_work(thpool, threaded_equihash, &thread_args[i]);
+      thpool_add_work (thpool, threaded_equihash, &thread_args[i]);
     }
 
-    thpool_wait(thpool);
-    total_time = get_time() - start_time;
+  thpool_wait (thpool);
+  total_time = get_time () - start_time;
 
-    fprintf(stdout, "num concurrent threads: %u\n", num_threads);
-    fprintf(stdout, "min. time: %lf\n", min_time);
-    fprintf(stdout, "max. time: %lf\n", max_time);
-    fprintf(stdout, "avg. time: %lf\n", total_time / iterations);
-    fprintf(stdout, "tot. time: %lf\n", total_time);
-    fprintf(stdout, "%lf sol/s\n", n_solutions / total_time);
-    fprintf(stdout, "total solutions: %zu\n", n_solutions);
+  fprintf (stdout, "num concurrent threads: %u\n", num_threads);
+  fprintf (stdout, "min. time: %lf\n", min_time);
+  fprintf (stdout, "max. time: %lf\n", max_time);
+  fprintf (stdout, "avg. time: %lf\n", total_time / iterations);
+  fprintf (stdout, "tot. time: %lf\n", total_time);
+  fprintf (stdout, "%lf sol/s\n", n_solutions / total_time);
+  fprintf (stdout, "total solutions: %zu\n", n_solutions);
 
-    for(size_t i = 0; i < num_threads*2; ++i) {
-        free(thread_args[i].src);
-        free(thread_args[i].dst);
-        for(size_t j = 0; j < 9; ++j) {
-            free(thread_args[i].indices[j]);
-        }
-        free(thread_args[i].indices);
+  for (size_t i = 0; i < num_threads * 2; ++i)
+    {
+      free (thread_args[i].src);
+      free (thread_args[i].dst);
+      for (size_t j = 0; j < 9; ++j)
+	{
+	  free (thread_args[i].indices[j]);
+	}
+      free (thread_args[i].indices);
     }
-    thpool_destroy(thpool);
+  thpool_destroy (thpool);
 
-    return 0;
+  return 0;
 }
--- opencl/equihash.h
+++ opencl/equihash.h
@@ -24,75 +24,82 @@
 #define NUM_BUCKETS (1 << NUM_COLLISION_BITS)/NUM_INDICES_PER_BUCKET
 #define DIGEST_SIZE 32
 
-typedef uint64_t digest_t[(DIGEST_SIZE + sizeof(uint64_t) - 1) / sizeof(uint64_t)];
+typedef uint64_t digest_t[(DIGEST_SIZE + sizeof (uint64_t) - 1) /
+			  sizeof (uint64_t)];
 
-typedef struct gpu_config {
-    unsigned flags;
+typedef struct gpu_config
+{
+  unsigned flags;
 
-    char* program_source_code;
-    size_t program_source_code_size;
+  char *program_source_code;
+  size_t program_source_code_size;
 
-    cl_program program;
+  cl_program program;
 
-    cl_platform_id platform_ids;
-    cl_uint n_platforms;
+  cl_platform_id platform_ids;
+  cl_uint n_platforms;
 
-    cl_device_id device_ids;
-    cl_uint n_devices;
+  cl_device_id device_ids;
+  cl_uint n_devices;
 
-    cl_context context;
-    cl_command_queue command_queue;
-
-
-    cl_kernel initial_bucket_hashing_kernel;
-
-    cl_kernel bucket_collide_and_hash_kernel;
-
-    cl_kernel produce_candidates_kernel;
-
-    cl_kernel produce_solutions_kernel;
+  cl_context context;
+  cl_command_queue command_queue;
 
 
+  cl_kernel initial_bucket_hashing_kernel;
 
-    // gpu variables below
-    cl_mem digests[2];
-    cl_mem new_digest_index;
-    cl_mem buckets;
-    cl_mem src_local_buckets;
-    //cl_mem dst_local_buckets;
-    cl_mem blake2b_digest;
-    cl_mem n_candidates;
-    cl_mem dst_candidates;
-    cl_mem n_solutions;
-    cl_mem dst_solutions;
-    cl_mem elements;
+  cl_kernel bucket_collide_and_hash_kernel;
+
+  cl_kernel produce_candidates_kernel;
+
+  cl_kernel produce_solutions_kernel;
+
+
+
+  // gpu variables below
+  cl_mem digests[2];
+  cl_mem new_digest_index;
+  cl_mem buckets;
+  cl_mem src_local_buckets;
+  //cl_mem dst_local_buckets;
+  cl_mem blake2b_digest;
+  cl_mem n_candidates;
+  cl_mem dst_candidates;
+  cl_mem n_solutions;
+  cl_mem dst_solutions;
+  cl_mem elements;
 } gpu_config_t;
 
 
-typedef struct element {
-    uint32_t digest_index;
-    uint32_t parent_bucket_data;
-    uint32_t a;
-    uint32_t b;
+typedef struct element
+{
+  uint32_t digest_index;
+  uint32_t parent_bucket_data;
+  uint32_t a;
+  uint32_t b;
 } element_t;
 
-typedef struct bucket {
-    element_t data[NUM_INDICES_PER_BUCKET/8 * 28];
-    volatile unsigned size;
+typedef struct bucket
+{
+  element_t data[NUM_INDICES_PER_BUCKET / 8 * 28];
+  volatile unsigned size;
 } bucket_t;
 
-typedef struct src_local_bucket {
-    element_t data[17];
+typedef struct src_local_bucket
+{
+  element_t data[17];
 } src_local_bucket_t;
 
-typedef struct dst_local_bucket {
-    element_t data[128];
+typedef struct dst_local_bucket
+{
+  element_t data[128];
 } dst_local_bucket_t;
 
 
-size_t equihash(uint32_t*, crypto_generichash_blake2b_state*, gpu_config_t* base_config);
-void equihash_init(gpu_config_t* config);
-void equihash_cleanup(gpu_config_t* config);
+size_t equihash (uint32_t *, crypto_generichash_blake2b_state *,
+		 gpu_config_t * base_config);
+void equihash_init (gpu_config_t * config);
+void equihash_cleanup (gpu_config_t * config);
 
 
 #endif
