a759cec22d7d56c6c09b00a13f3c1688872d57b2
David <david.jaenson@gmail.com>
2016-11-18 19:35:21 +0100

    Added final output info, including sol/s for the opencl version

--- opencl/test-equihash.c
+++ opencl/test-equihash.c
@@ -10,6 +10,11 @@
 #include <assert.h>
 #include "equihash.h"
 
+double get_time() {
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return ts.tv_sec + ts.tv_nsec / 1000000000.0;
+}
 
 
 
@@ -30,11 +35,15 @@ int main(int argc, char **argv)
   crypto_generichash_blake2b_init(&curr_state, (const unsigned char *) "", 0,
                                   (512 / 200) * 200 / 8);
 
+  size_t total_solutions = 0;
+  double start_time = get_time();
+
   for (uint32_t i = 0; i < n_iterations; ++i) {
     crypto_generichash_blake2b_state state = curr_state;
     crypto_generichash_blake2b_update(&state, (const uint8_t *) &i, 4);
     uint32_t dst_indices[20 * 512];
     size_t n_solutions = equihash(dst_indices, &state, config);
+    total_solutions += n_solutions;
 
     for (size_t i = 0; i < n_solutions; ++i) {
       for (size_t k = 0; k < 512; ++k) {
@@ -43,6 +52,12 @@ int main(int argc, char **argv)
       printf("\n\n");
     }
   }
+  double total_time = get_time() - start_time;
+  fprintf(stdout, "avg. time: %lf\n", total_time / n_iterations);
+  fprintf(stdout, "tot. time: %lf\n", total_time);
+  fprintf(stdout, "%lf sol/s\n", total_solutions / total_time);
+  fprintf(stdout, "total solutions: %zu\n", total_solutions);
+
   equihash_cleanup(config);
   free(config);
   return 0;
